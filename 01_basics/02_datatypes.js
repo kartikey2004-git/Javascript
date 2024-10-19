9a96998aca7e5138879b5ea7a42b9fc1d2676633
Kartikey bhatnagar <kartikeybhatnagar247@gmail.com>
2024-10-19 17:52:16 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,34 @@
+"use strict"; // treat all js code as newer version
+
+// alert("hello");
+// alert ( 3+3);
+// we are using node.js not browser
+
+console.log(3 + 3);// code readibility should be high
+console.log("hitesh");
+// mdn mozilla webdocs 
+//tc39 ecmascript documentation
+
+let name = "hitesh"
+let age = 18 
+let isLoggedIn = false
+let state ;
+
+//  number =>  2 to the power  53 range hoti hai
+// bigint 
+// string : single or double quote
+//boolean : true or false
+// null => standalone value
+// undefined =>
+// symbol for detect uniqueness  used in react or figma tools 
+
+
+// object 
+
+console.log(typeof"goli");
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
+
+
