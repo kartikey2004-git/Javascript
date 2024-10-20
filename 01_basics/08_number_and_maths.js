c0baf960a4ff7ad262dbd38e0246042ec92cc847
Kartikey bhatnagar <kartikeybhatnagar247@gmail.com>
2024-10-20 12:03:36 +0000

    number and maths

---
+++ 01_basics/08_number_and_maths.js
@@ -0,0 +1,35 @@
+// const score = 400
+// console.log(score)
+// // for explicitly define datatype
+// const balance = new Number (100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 1123.56677;
+// console.log(otherNumber.toPrecision(4));
+
+
+// const hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+// *********************** Maths ************
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.3));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4,44,34,23));
+// console.log(Math.max(4,44,34,23));
+
+console.log(Math.random())
+console.log(Math.floor(Math.random()*10) + 1);
+// value between 1 and 0 
+
+const max = 50;
+const min = 40;
+
+console.log(Math.floor(Math.random() * (max - min + 1))
+ + min)
