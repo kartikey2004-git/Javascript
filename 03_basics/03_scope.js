c3246a99104c44b98ccc96e3cebe4c0909ea0a24
Kartikey bhatnagar <kartikeybhatnagar247@gmail.com>
2024-10-22 17:05:09 +0000

    scope and hoisting

--- 03_basics/03_scope.js
+++ 03_basics/03_scope.js
@@ -1,21 +1,21 @@
-// var c = 300;
+var c = 300;
 
 // scope in js // if ke  jo bhi andar hai wo block scope hai
-// let a = 300
-// if (true) {
-//   let a = 10;
-//   const b = 20;
-//   console.log("INNER :",a)
-// }
+let a = 300
+if (true) {
+  let a = 10;
+  const b = 20;
+  console.log("INNER :",a)
+}
 
-// for (let i = 0; i < array.length; i++) {
-//     const element = array[i];   
-// }
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];   
+}
 
 
-// console.log(a);
-// console.log(b);
-// console.log(c);
+console.log(a);
+console.log(b);
+console.log(c);
 
 // global scope ki priority jyada hoti hai program mein , but scope mein local var ki jyada hoti hai priority 
 
@@ -32,7 +32,7 @@ function one(){
         const website = "youtube"
         // console.log(userName)
     }
-    // console.log(website)
+    console.log(website)
 
      two()
 }
@@ -45,7 +45,7 @@ if(true){
     const userName = "hitesh"
     if (userName === "hitesh"){
         const website = "youtube"
-        // console.log(userName + website);
+        console.log(userName + website);
     }
     // console.log(website)
 }
