6545dbc6b142c2af47a5a9c86e59a2e56a13a3dc
Kartikey bhatnagar <kartikeybhatnagar247@gmail.com>
2024-11-05 12:07:34 +0000

    oops commit

--- 10_classes_and_oops/oops.js
+++ 10_classes_and_oops/oops.js
@@ -18,15 +18,36 @@ console.log(this); // return {} in global context in terminal
 
 // for current context this keyword is used
 
-const user2 = {
-  username: "kartik",
-  loginCount: 8,
-  signedIn: true,
+// const user2 = {
+//   username: "kartik",
+//   loginCount: 8,
+//   signedIn: true,
 
-  getUserDetails : function(){
-    // console.log("got user details from database");
-    // console.log(`Username: ${this.username}`);
-    console.log(this); // current context
-  }
+//   getUserDetails : function(){
+//     // console.log("got user details from database");
+//     // console.log(`Username: ${this.username}`);
+//     console.log(this); // current context
+//   }
+// }
+
+
+
+// constructor functions (new) keyword ki importance
+// allows  us to create multiple instances fromsingle object literal
+
+// const promiseOne = new Promise()
+
+function User(username,loginCount,isLoggedIn){
+  this.username = username; // current context 
+  this.loginCount = loginCount;
+  this.isLoggedIn = isLoggedIn;
+
+  return(this); // implicitly return define hota hi hai
 }
-// constructor functions
+
+const userOne = new User("kartik",8,true)
+const userTwo = new User("kartikey",12,false) // ye value overwrite krdi saari ki saari
+
+console.log(userOne)
+console.log(userTwo)
+
